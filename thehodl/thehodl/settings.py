ab4a102289d2f358807d5d7030ab321fdde61c57
Robin Rheem <robinrheem@gmail.com>
2022-02-22 23:57:20 +0900

    Install debug toolbar

--- thehodl/thehodl/settings.py
+++ thehodl/thehodl/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "thehodl.urls"
